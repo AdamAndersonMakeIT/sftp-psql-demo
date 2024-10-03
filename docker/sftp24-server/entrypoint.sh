0b62f43ac9fd11996044c0938129a2c2ee5b028d
Adam Anderson <adam.anderson@makeit.com>
2024-10-03 14:47:58 -0700

    try more things

--- docker/sftp24-server/entrypoint.sh
+++ docker/sftp24-server/entrypoint.sh
@@ -9,32 +9,31 @@ TABLENAME=sftp_users
 
 cat <<EOF > /etc/nss-pgsql.conf
 connectionstring = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDBPASSWORD connect_timeout=2
-getpwnam = SELECT login AS username,"password" as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME WHERE login = \$1
-getpwuid = SELECT login AS username,"password" as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME WHERE uid = \$1
+getpwnam = SELECT login AS username,'x' as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME WHERE login = \$1
+getpwuid = SELECT login AS username,'x' as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME WHERE uid = \$1
 getgroupmembersbygid = SELECT login AS username FROM $TABLENAME WHERE uid = \$1
-getgrnam = SELECT login AS groupname,'x' as passwd,uid as gid FROM $TABLENAME WHERE login = \$1
-getgrgid = SELECT login AS groupname,'x' as passwd,uid as gid FROM $TABLENAME WHERE uid = \$1
+getgrnam = SELECT login AS groupname,'x' as passwd,uid as gid,ARRAY[login] AS members FROM $TABLENAME WHERE login = \$1
+getgrgid = SELECT login AS groupname,'x' as passwd,uid as gid,ARRAY[login] AS members FROM $TABLENAME WHERE uid = \$1
 groups_dyn = SELECT uid AS gid FROM $TABLENAME WHERE login = \$1 AND uid <> \$2
-allusers = SELECT login AS username,"password" as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME
-allgroups = SELECT login AS groupname,'x' as passwd,uid as gid FROM $TABLENAME
+allusers = SELECT login AS username,'x' as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME
+allgroups = SELECT login AS groupname,'x' as passwd,uid as gid,ARRAY[login] AS members FROM $TABLENAME
 EOF
 
 cat <<EOF > /etc/nss-pgsql-root.conf
+gshadowconnectionstring = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDBPASSWORD connect_timeout=2
 shadowconnectionstring = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDBPASSWORD connect_timeout=2
 connectionstring = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDBPASSWORD connect_timeout=2
-shadowbyname = SELECT login AS shadow_name, "password" AS shadow_passwd, 14087 AS shadow_lstchg, 0 AS shadow_min, 99999 AS shadow_max, 7 AS shadow_warn, _ AS shadow_inact, _ AS shadow_expire, _ AS shadow_flag FROM $TABLENAME WHERE login = \$1
-shadow = SELECT login AS shadow_name, "password" AS shadow_passwd, 14087 AS shadow_lstchg, 0 AS shadow_min, 99999 AS shadow_max, 7 AS shadow_warn, _ AS shadow_inact, _ AS shadow_expire, _ AS shadow_flag FROM $TABLENAME
+shadowbyname = SELECT login AS shadow_name, "password" AS shadow_passwd, 14087 AS shadow_lstchg, 0 AS shadow_min, 99999 AS shadow_max, 7 AS shadow_warn, NULL AS shadow_inact, NULL AS shadow_expire, NULL AS shadow_flag FROM $TABLENAME WHERE login = \$1
+shadow = SELECT login AS shadow_name, "password" AS shadow_passwd, 14087 AS shadow_lstchg, 0 AS shadow_min, 99999 AS shadow_max, 7 AS shadow_warn, NULL AS shadow_inact, NULL AS shadow_expire, NULL AS shadow_flag FROM $TABLENAME
+gshadow = SELECT login AS shadow_name, 'x' AS shadow_passwd, 14087 AS shadow_lstchg, 0 AS shadow_min, 99999 AS shadow_max, 7 AS shadow_warn, NULL AS shadow_inact, NULL AS shadow_expire, NULL AS shadow_flag FROM $TABLENAME
 EOF
 chmod 0600 /etc/nss-pgsql-root.conf
 
 cat <<EOF > /etc/pam_pgsql.conf
-host = $DBIP
-database = $DBNAME
-user = $FTPDBUSER
-password = $FTPDBPASSWORD
-table = $TABLENAME
-user_column = login
-pwd_column = password
+connect = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDBPASSWORD connect_timeout=2
+auth_query = select "password" from $TABLENAME where login = %u
+acct_query = select false, false, false from $TABLENAME where login = %u
+
 pw_type = crypt
 debug = 1
 EOF
