633e0288f55b4073aeb23983f415790eb392e5d2
Adam Anderson <adam.anderson@makeit.com>
2024-10-03 13:19:52 -0700

    Add some stuff I saw

--- docker/sftp24-server/entrypoint.sh
+++ docker/sftp24-server/entrypoint.sh
@@ -12,16 +12,20 @@ connectionstring = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDB
 getpwnam = SELECT login AS username,"password" as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME WHERE login = \$1
 getpwuid = SELECT login AS username,"password" as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME WHERE uid = \$1
 getgroupmembersbygid = SELECT login AS username FROM $TABLENAME WHERE uid = \$1
-getgrnam = SELECT login AS groupname,'x',uid as gid,ARRAY[login] AS members FROM $TABLENAME WHERE login = \$1
-getgrgid = SELECT login AS groupname,'x',uid as gid,ARRAY[login] AS members FROM $TABLENAME WHERE uid = \$1
+getgrnam = SELECT login AS groupname,'x' as passwd,uid as gid FROM $TABLENAME WHERE login = \$1
+getgrgid = SELECT login AS groupname,'x' as passwd,uid as gid FROM $TABLENAME WHERE uid = \$1
 groups_dyn = SELECT uid AS gid FROM $TABLENAME WHERE login = \$1 AND uid <> \$2
+allusers = SELECT login AS username,"password" as passwd,login as gecos,'/home/' || login AS homedir,'/bin/bash' as shell,uid,uid as gid FROM $TABLENAME
+allgroups = SELECT login AS groupname,'x' as passwd,uid as gid FROM $TABLENAME
 EOF
 
 cat <<EOF > /etc/nss-pgsql-root.conf
+shadowconnectionstring = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDBPASSWORD connect_timeout=2
 connectionstring = hostaddr=$DBIP dbname=$DBNAME user=$FTPDBUSER password=$FTPDBPASSWORD connect_timeout=2
 shadowbyname = SELECT login AS shadow_name, "password" AS shadow_passwd, 14087 AS shadow_lstchg, 0 AS shadow_min, 99999 AS shadow_max, 7 AS shadow_warn, _ AS shadow_inact, _ AS shadow_expire, _ AS shadow_flag FROM $TABLENAME WHERE login = \$1
 shadow = SELECT login AS shadow_name, "password" AS shadow_passwd, 14087 AS shadow_lstchg, 0 AS shadow_min, 99999 AS shadow_max, 7 AS shadow_warn, _ AS shadow_inact, _ AS shadow_expire, _ AS shadow_flag FROM $TABLENAME
 EOF
+chmod 0600 /etc/nss-pgsql-root.conf
 
 cat <<EOF > /etc/pam_pgsql.conf
 host = $DBIP
