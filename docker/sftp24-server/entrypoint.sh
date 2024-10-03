795859e51a87c8708b93a68eae8de736da11a11f
Adam Anderson <adam.anderson@makeit.com>
2024-10-03 11:23:58 -0700

    fix1

--- docker/sftp24-server/entrypoint.sh
+++ docker/sftp24-server/entrypoint.sh
@@ -39,8 +39,18 @@ EOF
 # Enable SFTP
 
 sed -i '/Subsystem/d' /etc/ssh/sshd_config
+sed -i '/X11Forwarding/d' /etc/ssh/sshd_config
+sed -i '/AllowTcpForwarding/d' /etc/ssh/sshd_config
+#sed -i '/ForceCommand/d' /etc/ssh/sshd_config
+sed -i '/ChrootDirectory/d' /etc/ssh/sshd_config
+sed -i '/LogLevel/d' /etc/ssh/sshd_config
+
 echo Subsystem sftp internal-sftp >> /etc/ssh/sshd_config
+echo X11Forwarding no >> /etc/ssh/sshd_config
+echo AllowTcpForwarding no >> /etc/ssh/sshd_config
+#echo ForceCommand internal-sftp >> /etc/ssh/sshd_config
 echo ChrootDirectory /home/%u >> /etc/ssh/sshd_config
+echo LogLevel VERBOSE >> /etc/ssh/sshd_config
 
 # Start ssh server
 
